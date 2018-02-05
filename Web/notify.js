b02b8bea3aeaacf41864946fde71fc37d417ff58
Tamir Abutbul <34707669+Tamir198@users.noreply.github.com>
2018-02-05 16:35:56 +0200

    UI fixes
    minor UI changing , and a method that marks the pressed nav bar item , and if that nav bar item if "search us on facebook" - than open a new tab at facebook page

--- Web/notify.js
+++ Web/notify.js
@@ -1,11 +1,22 @@
 $(()=>{
-	/*a method to change the cliked nav bar item class to "active" , and mark him by that*/
-	$(".nav>li").on("click", function() {
+	/*a method that changes the clicked nav bar item class to "active" and mark him.
+	in addition checks if the current pressed nav bar item is not a link to external website(so preventDefault 
+	method will not be called on him)*/
+		
+	$(".nav li").click(function(e){
+		if(e.target.id = ""){
+		e.preventDefault();
+	
+		console.log(n+1)
+		}else{
+		console.log("TODO : put a method for changing section content")
 		$(".nav li").removeClass("active");
+		$(".nav li").preventDefault;
 		$(this).addClass("active");
-		});
+	}});
 	
 });
+
 
 
 
