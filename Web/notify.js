afc35ca47d55bdf2e9cc00685e30c5238daf4aa0
Tamir Abutbul <34707669+Tamir198@users.noreply.github.com>
2018-02-07 14:32:45 +0200

    adding files and working with xampp
    fist work with xampp , all of the navigation bar items are now coming from server.

--- Web/notify.js
+++ Web/notify.js
@@ -2,18 +2,22 @@ $(()=>{
 	/*a method that changes the clicked nav bar item class to "active" and mark him.
 	in addition checks if the current pressed nav bar item is not a link to external website(so preventDefault 
 	method will not be called on him)*/
-		
-	$(".nav li").click(function(e){
-		if(e.target.id = ""){
-		e.preventDefault();
+
 	
-		console.log(n+1)
-		}else{
-		console.log("TODO : put a method for changing section content")
+	console.log(firstScreen);
+	$(".nav li").click(function(e){
+		e.preventDefault();
+		changeHtml(e.target.href);
 		$(".nav li").removeClass("active");
-		$(".nav li").preventDefault;
 		$(this).addClass("active");
-	}});
+	});
+	
+	function changeHtml(url){
+		$.get(url, (html)=>{
+			$('#section').html(html);
+		});
+		console.log(url);
+	}
 	
 });
 
