af93bfc2d83c4f13d0ce1ffa6da6778c901c1706
DannykZ <31693501+DannykZ@users.noreply.github.com>
2018-02-14 00:05:22 +0200

    updated logout button

--- Noteify-master/Noteify-master/Web/notify.js
+++ Noteify-master/Noteify-master/Web/notify.js
@@ -1,4 +1,4 @@
-	var USER_NAME = localStorage.getItem("CurrentUser");	
+var USER_NAME = localStorage.getItem("CurrentUser");	
 $(()=>{
 	console.log("on load was called");
 	const pages = {};
@@ -38,39 +38,57 @@ $(()=>{
 		$("#login_1").show(500);
 	});
 	}
-	
+	//Login button!
 	 function btnLoginListener(){
 		 	$("#btnLogin").click((e)=>{
 			e.preventDefault();
 			console.log("dani method working");
 			var l = window.localStorage;
-			var key=JSON.parse(l.getItem($("#LoginEmail").val()));
+			var key = JSON.parse(l.getItem($("#LoginEmail").val()));
+			
 			if(key["pass"] === $("#LoginPassword").val()){
-				l.setItem("CurrentUser" ,$("#LoginEmail").val());
+				l.setItem("CurrentUser", $("#LoginEmail").val());
 				console.log(l.getItem("CurrentUser"));
 				moveToNotes();
-			}else{
-				alert("Wrong user inputs","Error");
+				$("#notesPage").html("Logout");
+				logOut();
+				
 			}
-				});
-	 }	
-
+			else {
+				$("#passVali").text("Invalid Password");
+			
+			}
+		});
+	 }
+	//Logout button!
+	function logOut(){
+		$(notesPage).click(()=>{
+			
+			if($("#notesPage").html("Login")){
+				console.log("success");
+				localStorage.removeItem("CurrentUser");		
+			}
+			else{
+				alert("failed");
+			}
+		})
 	
+	}
 
-	
+		
 });
-	
+
 	//login and registration java script in addition
 	function register(){
 		$("#btnRegister").click((e)=>{	
 			e.preventDefault();
 			var s = window.localStorage
 			var ArrayData = {};
-		    ArrayData={"pass": $("#InputPassword").val(), "name": $("#InputName").val(),"tasks":[{"bdika":"bdika"}]};
+		    ArrayData = {"pass": $("#InputPassword").val(), "name": $("#InputName").val(),"tasks":[{"bdika":"bdika"}]};
 		    s.setItem($("#InputEmail").val(), JSON.stringify(ArrayData));
 			$("#reg_1").hide(500);
 			$("#login_1").show(500);
-			});
+		});
 	}
 	
 		//Change Section Data To Note.html
@@ -82,71 +100,43 @@ $(()=>{
 			getData();
 		});
 	}       
-
-
-
 	
 	//Notes java script starts from here(Shimshon).
 	
 	//Add task to the list.
 	function addTask(){	
-		var USER_NAME = localStorage.getItem("CurrentUser");
 		var inputTask = $("#input").val();
 		var mainStorage = JSON.parse(localStorage.getItem(USER_NAME));
 		if(mainStorage != null){
+			
 			storage = mainStorage["tasks"];
 			$("#list").append("<li><input type='checkbox' onchange='changeColor(this)'></input>"+inputTask+"</li>");
-		if(storage==null){
-			storage= [{[$("#list>li").length]:inputTask,"check":false}]
-		}else{
+			
+			if(storage == null){
+				storage= [{[$("#list>li").length]:inputTask,"check":false}]
+			}else{
 			storage.push( {[$("#list>li").length]:inputTask,"check":false});
 		}
-			mainStorage["tasks"]=storage
+			mainStorage["tasks"] = storage
 			localStorage.setItem(USER_NAME,JSON.stringify(mainStorage));
 			$("#input").val("");
-		}else{
+		}
+		else{
 			alert("You need to register to use this feature");
 		}
 		}
-		
-	//Delete checked tasks.	
-	function deleteMarked(elemnt){
-		var USER_NAME = localStorage.getItem("CurrentUser");
-		 var mainStorage = JSON.parse(localStorage.getItem(USER_NAME));
-		 if(mainStorage !=null){
-		 storage = mainStorage["tasks"];
-		var tasks = $("#list").children();
-		for (i=0;i<storage.length;i++){
-			if(storage[i]["check"]){
-				console.log("deleted"+i);
-				tasks[i].remove();
-				for(j=0;j<(storage.length-i);j++){
-					console.log(i);
-					console.log(storage[i+j]);
-				storage[i+j] = storage[i+(j+1)];	
-				}
-				storage.splice(storage.length-1,1);
-			}
-						}
-						mainStorage["tasks"]=storage;
-						localStorage.setItem(USER_NAME,JSON.stringify(mainStorage));
-						}else alert("You need to register to use this feature");
-											
-		 }
-		
 	
 	//Remove all tasks.
 		function clearList(){
-			var USER_NAME = localStorage.getItem("CurrentUser");
-			$("#list").html("");
-			 var mainStorage = JSON.parse(localStorage.getItem(USER_NAME));
-			 if(mainStorage !=null){
-				storage=mainStorage["tasks"];
-				storage = null;
-				mainStorage["tasks"] = storage;
-				localStorage.setItem(USER_NAME,JSON.stringify(mainStorage));
-				console.log("List tasks were deleted.");
-			}else alert("You need to register to use this feature");
+		$("#list").html("");
+		 var mainStorage = JSON.parse(localStorage.getItem(USER_NAME));
+		 if(mainStorage != null){
+			storage = mainStorage["tasks"];
+			storage = null;
+			mainStorage["tasks"] = storage;
+			localStorage.setItem(USER_NAME,JSON.stringify(mainStorage));
+			console.log("List tasks were deleted.");
+		}else alert("You need to register to use this feature");
 		}
 		
 		//Change note color.
@@ -160,9 +150,9 @@ $(()=>{
 				var index = 0;
 				for (i=0;i<storage.length;i++){
 					lineData = storage[i];
-					console.log(lineData[Object.keys(lineData)[0]]);
+					console.log(lineData[i+1]);
 					console.log(elemnt.parentNode.textContent);
-					if(lineData[Object.keys(lineData)[0]] === elemnt.parentNode.textContent){
+					if(lineData[i+1]===elemnt.parentNode.textContent){
 						console.log("true");
 						index=i;
 						break;			
@@ -184,7 +174,7 @@ $(()=>{
 	//Load tasks.
 		function getData(){
 			var USER_NAME = localStorage.getItem("CurrentUser");
-			var mainStorage=JSON.parse(localStorage.getItem(USER_NAME));
+			var mainStorage = JSON.parse(localStorage.getItem(USER_NAME));
 			if(mainStorage != null){
 				storage = mainStorage["tasks"];
 			if (storage === undefined){
@@ -194,12 +184,11 @@ $(()=>{
 				var lineData = storage[i];
 				var color="black";
 				var checked=""
-				console.log(lineData[Object.keys(lineData)[0]]);
 				if(lineData["check"]){
 					color="green";
 					checked="checked";
 				}
-				$("#list").append("<li style='color:"+color+";'><input type='checkbox' onchange='changeColor(this)' "+checked+"></input>"+lineData[Object.keys(lineData)[0]]+"</li>");
+				$("#list").append("<li style='color:"+color+";'><input type='checkbox' onchange='changeColor(this)' "+checked+"></input>"+lineData[i+1]+"</li>");
 			}
 			}else console.log("You need to register to use this feature.")
 		}
